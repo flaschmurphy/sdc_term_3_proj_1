63cbf288e86c508c05117e0aba4d22af95d21377
Ciaran Murphy <ciaranrmurphy@gmail.com>
2018-07-01 16:53:19 +0200

    Fully working version

--- src/main.cpp
+++ src/main.cpp
@@ -205,8 +205,14 @@ int main() {
   // Start in lane 1
   int lane = 1;
 
-  // Have a reference velocity (mph) to target
+  // Have a reference velocity (mph) to target. This is set to zero initially
+  // and incrementally increased at the beginning of the simulation
   double ref_vel = 0;
+
+  // This is used to stabilize the perceived state of the lanes (occupied or free)
+  deque<int> left_lane_occupancy = {0, 0, 0, 0, 0};
+  deque<int> middle_lane_occupancy = {0, 0, 0, 0, 0};
+  deque<int> right_lane_occupancy = {0, 0, 0, 0, 0};
 
   h.onMessage([&map_waypoints_x,
                &map_waypoints_y,
@@ -214,8 +220,12 @@ int main() {
                &map_waypoints_dx,
                &map_waypoints_dy,
                &lane,
+               &left_lane_occupancy,
+               &middle_lane_occupancy,
+               &right_lane_occupancy,
                &ref_vel]
                (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -274,118 +284,241 @@ int main() {
             // Store the indices of any cars that are close to the ego car
             vector<int> close_cars;
 
+            // Scalar to convert miles per hour to meters per sec
+            float mph2mps= 0.44704; 
+
+            // the "front_car" is the car that's directly in front of our car in the same lane 
             int front_car_idx = 999; // just some large number
             int front_car_distance = 999; 
             int front_car_speed = 999;
 
-            double nearest_car_lane0 = 999;
-            double nearest_car_lane2 = 999;
-            int best_of_lane_0_or_2 = 0;
-
-            int check_car_lane;
+            double free_space_lane0 = 999;
+            double free_space_lane1 = 999;
+            double free_space_lane2 = 999;
+            int best_of_lane_0_or_2 = 0; // arbitrarily prefer lane 0 over lane 2
 
             bool lane0_free = true;
             bool lane1_free = true;
             bool lane2_free = true;
 
+            // Define the min safe distance in front and behind the ego car
             int min_rear_clearance = 20;
             int min_forward_clearance = 15;
 
-            //cout << "Ego s:" << car_s << endl;
-            //for (int i=0; i<sensor_fusion.size(); i++)
-            //    cout << sensor_fusion[i][5] << "|";
-            //cout << endl;
+            // Minimum safe distance to the car in front in the same lane
+            int min_samelane_clearance = min_forward_clearance+10;
 
+            // Max acceleration and deceleration. Both are allowed to be as much as 10m/s^2
+            float max_acceleration = 1.7;
+            float max_deceleration = 1.9;
+            float actual_deceleration = 0;
+
+            // Differnce in velocity between ego and vehicle in front, actual and normalized
+            double vel_diff = 0;
+            double vel_diff_norm = 0;
+
+            // Examine all the sensor fusion data (i.e. all the other cars on the road)
             for (int i=0; i<sensor_fusion.size(); i++)
             {
+                int check_car_lane;
                 double vx = sensor_fusion[i][3];
                 double vy = sensor_fusion[i][4];
-                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_v = sqrt(vx*vx+vy*vy);
                 double check_car_s = sensor_fusion[i][5];
-                //check_car_s += ((double)prev_size*0.2*check_speed);
                 float check_car_d = sensor_fusion[i][6];
 
-                if (check_car_d < 4) // 4 is the lane width
+                // The project walk through included the line below which takes
+                // into account the fact that we need to consider the future
+                // state of all vehicles not just their instantaneous state.
+                // However during my testing I found this code actually makes
+                // the performance worse not better
+                //check_car_s += ((double)prev_size*0.2*check_car_v);
+
+                // Left lane
+                if (check_car_d < 4.0) // 4 is the lane width, so <4 means lane 0
                 {
                     check_car_lane = 0;
                     if ((check_car_s >= car_s) && ((check_car_s - car_s) < min_forward_clearance))
-                    {
                         lane0_free = false;
-                        if ((check_car_s - car_s) < nearest_car_lane0)
-                            nearest_car_lane0 = check_car_s - car_s;
-                    }
                     else if ((check_car_s < car_s) && ((car_s - check_car_s) < min_rear_clearance))
                         lane0_free = false;
+                    if (abs(check_car_s - car_s) < free_space_lane0)
+                        free_space_lane0 = abs(check_car_s - car_s);
+
                 }
-                else if (check_car_d >= 4  && check_car_d <= 8)
+                // Middle lane
+                else if (check_car_d >= 4.0  && check_car_d <= 8.0)
                 {
+                    // Check state of middle lane
                     check_car_lane = 1;
                     if ((check_car_s >= car_s) && ((check_car_s - car_s) < min_forward_clearance))
                         lane1_free = false;
                     else if ((check_car_s < car_s) && ((car_s - check_car_s) < min_rear_clearance))
                         lane1_free = false;
+
+                    // Update the free space value
+                    if (abs(check_car_s - car_s) < free_space_lane1)
+                        free_space_lane1 = abs(check_car_s - car_s);
                 }
-                else if (check_car_d > 8)
+                // Right lane
+                else if (check_car_d > 8.0)
                 {
                     check_car_lane = 2;
                     if ((check_car_s >= car_s) && ((check_car_s - car_s) < min_forward_clearance))
-                    {
                         lane2_free = false;
-                        if ((check_car_s - car_s) < nearest_car_lane0)
-                            nearest_car_lane0 = check_car_s - car_s;
-                    }
                     else if ((check_car_s < car_s) && ((car_s - check_car_s) < min_rear_clearance))
                         lane2_free = false;
+                    if (abs(check_car_s - car_s) < free_space_lane2)
+                        free_space_lane2 = abs(check_car_s - car_s);
                 }
 
+                // Deal with any car that is in our current lane
                 if (check_car_lane == lane)
                 {
-                    // If the car is behind us, just ignore it.
+                    // If the car is behind us, just ignore it. This is ok for
+                    // any car that's in the same lane as the ego vehicle
                     if (check_car_s < car_s)
                         continue;
 
-                    if ((check_car_s > car_s) && ((check_car_s - car_s) < 25))
+                    if ((check_car_s > car_s) && ((check_car_s - car_s) < min_samelane_clearance))
                     {
                         close_cars.push_back(i);
-                        int distance = check_car_s - car_s;
-                        if (distance < front_car_distance) 
+                        if ((check_car_s - car_s) < front_car_distance)
                         {
                             front_car_idx = i;
-                            front_car_speed = check_speed;
-                            front_car_distance = distance;
+                            front_car_speed = check_car_v;
+                            front_car_distance = check_car_s - car_s;
                         }
                     }
                 }
             }
 
-            // Print some status info. A printed 1 means the lane is not free
-            cout << !lane0_free << " | " << !lane1_free << " | " << !lane2_free << endl;
+            // Update the occupancy deques 
+            left_lane_occupancy.pop_front();
+            middle_lane_occupancy.pop_front();
+            right_lane_occupancy.pop_front();
 
+            if (lane0_free)
+                left_lane_occupancy.push_back(0);
+            else
+                left_lane_occupancy.push_back(1);
+
+            if (lane1_free)
+                middle_lane_occupancy.push_back(0);
+            else
+                middle_lane_occupancy.push_back(1);
+
+            if (lane2_free)
+                right_lane_occupancy.push_back(0);
+            else
+                right_lane_occupancy.push_back(1);
+
+            // By default best_of_lane_0_or_2 is already set to 0
+            if (free_space_lane2 > free_space_lane0)
+                best_of_lane_0_or_2 = 2;
+
+            // Calculate the sliding window state of all lanes
+            int lane0_sum = 0;
+            for (int n : left_lane_occupancy)
+                lane0_sum += n;
+
+            int lane1_sum = 0;
+            for (int n : middle_lane_occupancy)
+                lane1_sum += n;
+
+            int lane2_sum = 0;
+            for (int n : right_lane_occupancy)
+                lane2_sum += n;
+
+            // Prefer the middle lane when reasonable
+            if ((lane == 0 || lane == 2) && (lane1_sum == 0))
+                {
+                    // If there is a lane change already inprogress don't initiate another one
+                    if (((car_d >= 1) && (car_d <= 3)) || ((car_d >= 9) && (car_d <= 11)))
+                    {
+                        if ((lane == 0) && (free_space_lane0 < free_space_lane1))
+                            lane = 1;
+                        else if ((lane == 2) && (free_space_lane2 < free_space_lane1))
+                            lane = 1;
+                    }
+                }
+
+            // Decide whether or not to change lanes, change speed or maintain current state
             if (close_cars.size() > 0)
             {
-                if ((lane == 0 || lane == 2) && lane1_free)
-                    lane = 1;
-                else if (lane == 1 && lane2_free)
+                bool must_decelerate = true;
+                // We only need to check what to do if in lane 1 below since
+                // later on, if we are in any other lane we try to get back to
+                // lane 1 asap anyway. So all we need to do here is make sure
+                // we don't stay trapped in lane 1 unnecessarily.
+                if (lane == 1)
                 {
-                    best_of_lane_0_or_2 = 0;
-                    if (nearest_car_lane2 < nearest_car_lane0)
-                        best_of_lane_0_or_2 = 2;
-                    lane = best_of_lane_0_or_2;
+                    if ((best_of_lane_0_or_2 == 0) && (lane0_sum == 0))
+                    {
+                        // If there is a lane change already inprogress don't initiate another one
+                        if ((car_d >= 5) && (car_d <= 7))
+                        {
+                            lane = 0;
+                            must_decelerate = false;
+                        }
+                    }
+                    else if ((best_of_lane_0_or_2 == 2) && (lane2_sum == 0))
+                    {
+                        if ((car_d >= 5) && (car_d <= 7))
+                        {
+                            lane = 2;
+                            must_decelerate = false;
+                        }
+                    }
                 }
-                else if (lane == 1 && lane0_free)
-                    lane = 0;
-                else
-                    ref_vel -= 0.324;
-            } 
-            else if (ref_vel < 49.5) 
-            {
-                ref_vel += 0.224;
-            } 
 
-            // Regardless of whether or not there is a car in front, always
-            // prefer the middle lane
-            if ((lane == 0 || lane == 2) && lane1_free)
-                lane = 1;
+                // The case where we are in lane 0 or 2 is that earlier in the code we will
+                // have already initiated a lane change back to the middle lane if it were
+                // possible. So there's no need to check if the middle lane is free here again
+                if (must_decelerate) 
+                {
+                    // When it's necessary to decelerate, chose a rate of
+                    // deceleration that is equal to the max allowed scaled
+                    // down by a factor proportional to the difference in
+                    // velocity of the two cars
+                    vel_diff = abs(front_car_speed - (car_speed*mph2mps));
+                    vel_diff_norm = vel_diff / front_car_speed;
+                    actual_deceleration = abs((vel_diff_norm * max_deceleration));
+                    ref_vel -= actual_deceleration;
+                }
+            }
+            // There is no car close in front, so accelerate to max velocity,
+            // which is 50, but we need to stay a bit below to be on the safe side.
+            else if (ref_vel < 48.0)
+                ref_vel += max_acceleration;
+
+            //////////////////////////////////////////////////////////////////
+            //  Print some status info.
+            //////////////////////////////////////////////////////////////////
+
+            // Status of each lane, 1 means occupied and a * represents preference between lanes 0 and 2
+            cout << !lane0_free; if (best_of_lane_0_or_2 == 0) cout << "* "; else cout << "  ";
+            cout << !lane1_free << "  ";
+            cout << !lane2_free; if (best_of_lane_0_or_2 == 2) cout << "*| "; else cout << " | ";
+
+            // Occupancy history for the middle lane
+            for (int n : middle_lane_occupancy) cout << n << " "; cout << "| ";
+
+            // Ego d
+            cout << car_d << " | ";
+
+            // Speed of ego and car in front
+            cout << car_speed*mph2mps << " " << front_car_speed << " | ";
+
+            // Amount of free space in each of the 3 lanes
+            //cout << (int)free_space_lane0 << " " << (int)free_space_lane1 <<
+            //    " " << (int)free_space_lane2 << " | ";
+
+            // Distance to car in front, diff in velocity, normalized diff, planned deceleration
+            //cout << front_car_distance << " " << vel_diff << " " <<
+            //    vel_diff_norm << " " << actual_deceleration << " | ";
+
+            cout << endl << flush;
 
             //////////////////////////////////////////////////////////
             // End fusion data processing and lane change handling
